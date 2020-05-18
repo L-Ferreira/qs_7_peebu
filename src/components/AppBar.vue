b49728080d1f50e54fc6b64c74b499e68c64597a
luis ferreira <luispaulo_26@hotmail.com>
2020-05-18 19:07:09 +0100

    Added background to AppBar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar app fixed dark color="#58a7c5">
+  <v-app-bar app fixed dark color="#58a7c5" src="@/assets/banner.jpg">
     <v-spacer />
     <v-toolbar-title>
       <span class="display-1 flex-center">PeeBu</span>
