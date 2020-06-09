ce7147053f9de5bdc0c125ed7a9217fa30dbf3c8
luis ferreira <luispaulo_26@hotmail.com>
2020-06-09 22:05:17 +0100

    Added search bar to data table

--- src/components/TransactionDataTable.vue
+++ src/components/TransactionDataTable.vue
@@ -1,22 +1,31 @@
 <template>
-  <v-data-table
-    ref="table"
-    :headers="headers"
-    :items="this.expenses"
-    :search="search"
-    class="elevation-4"
-  >
-    <template v-slot:top>
-      <v-toolbar flat color="white">
-        <v-toolbar-title>My Expenses</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <!-- <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon> -->
-      <v-icon small @click="itemInfo(item)">mdi-information-outline</v-icon>
-    </template>
-  </v-data-table>
+  <div>
+    <v-data-table
+      ref="table"
+      :headers="headers"
+      :items="this.expenses"
+      :search="search"
+      class="elevation-4"
+    >
+      <template v-slot:top>
+        <v-toolbar flat color="white">
+          <v-toolbar-title>My Expenses</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <!-- <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon> -->
+        <v-icon small @click="itemInfo(item)">mdi-information-outline</v-icon>
+      </template>
+    </v-data-table>
+  </div>
 </template> 
 <script>
 export default {
