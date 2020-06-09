145bc2db17fb5763817003b1c3b342260b6501a6
luis ferreira <luispaulo_26@hotmail.com>
2020-06-09 17:54:03 +0100

    Added TransactionDataTable component

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
       <v-container>
         <h1>Welcome!</h1>
         <h2>Your current balance is: 12345 €</h2>
+        <br />
+        <TransactionDataTable />
       </v-container>
     </v-content>
   </v-app>
@@ -13,12 +15,14 @@
 
 <script>
 import AppBar from "@/components/AppBar.vue";
+import TransactionDataTable from "@/components/TransactionDataTable.vue";
 
 export default {
   name: "App",
 
   components: {
-    AppBar
+    AppBar,
+    TransactionDataTable
   },
 
   data: () => ({
---
+++ src/components/TransactionDataTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-data-table
+    ref="table"
+    :headers="headers"
+    :items="this.expenses"
+    :search="search"
+    class="elevation-4"
+  >
+    <template v-slot:top>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>My Expenses</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <!-- <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon> -->
+      <v-icon small @click="itemInfo(item)">mdi-information-outline</v-icon>
+    </template>
+  </v-data-table>
+</template> 
+<script>
+export default {
+  data: () => ({
+    editDialog: false,
+    infoDialog: false,
+    headers: [
+      {
+        text: "Entity",
+        align: "start",
+        value: "entity"
+      },
+      { text: "Amount", value: "amount" },
+      { text: "Date", value: "createdAt" },
+      { text: "Type", value: "type" },
+      { text: "Category", value: "category" },
+      { text: "Actions", value: "actions", sortable: false }
+    ]
+  })
+};
+</script>
