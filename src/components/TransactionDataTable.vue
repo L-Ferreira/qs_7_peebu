a2261763b1b55d06e8f1cee0041e2ab704bca283
Alexandre Silva <2180300@my.ipleiria.pt>
2020-06-11 18:55:52 +0100

    Added Transaction Details component

--- src/components/TransactionDataTable.vue
+++ src/components/TransactionDataTable.vue
@@ -1,86 +1,114 @@
 <template>
-  <div>
-    <v-data-table
-      ref="table"
-      :headers="headers"
-      :items="this.expenses"
-      :search="search"
-      class="elevation-4"
-    >
-      <template v-slot:top>
-        <v-toolbar flat color="white">
-          <v-toolbar-title>My Expenses</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <!-- <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon> -->
-        <v-icon small @click="itemInfo(item)">mdi-information-outline</v-icon>
-      </template>
-    </v-data-table>
-  </div>
-</template> 
+	<div>
+		<v-data-table
+			ref="table"
+			:headers="headers"
+			:items="this.expenses"
+			:search="search"
+			class="elevation-4"
+		>
+			<template v-slot:top>
+				<v-toolbar flat color="white">
+					<v-toolbar-title>My Expenses</v-toolbar-title>
+					<v-divider class="mx-4" inset vertical></v-divider>
+					<v-text-field
+						v-model="search"
+						append-icon="mdi-magnify"
+						label="Search"
+						single-line
+						hide-details
+					></v-text-field>
+					<TransactionInfoDialog
+						:info-dialog="infoDialog"
+						:edited-item="editedItem"
+						@infoDialogClosed="closeInfoDialog"
+					></TransactionInfoDialog>
+				</v-toolbar>
+			</template>
+			<template v-slot:item.actions="{ item }">
+				<!-- <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon> -->
+				<v-icon small @click="itemInfo(item)"
+					>mdi-information-outline</v-icon
+				>
+			</template>
+		</v-data-table>
+	</div>
+</template>
 
 <script>
 import axios from "axios";
+import TransactionInfoDialog from "./TransactionInfoDialog.vue";
+
 export default {
-  data: () => ({
-    editDialog: false,
-    infoDialog: false,
-    headers: [
-      {
-        text: "Entity",
-        align: "start",
-        value: "entity"
-      },
-      { text: "Amount", value: "amount" },
-      { text: "Date", value: "createdAt" },
-      { text: "Type", value: "type" },
-      { text: "Category", value: "category" },
-      { text: "Actions", value: "actions", sortable: false }
-    ],
-    expenses: [],
-    search: "",
-    editedIndex: -1,
-    editedItem: {
-      index: ""
-    },
+	components: {
+		TransactionInfoDialog,
+	},
 
-    startDate: null,
-    endDate: null,
+	data: () => ({
+		editDialog: false,
+		infoDialog: false,
+		headers: [
+			{
+				text: "Entity",
+				align: "start",
+				value: "entity",
+			},
+			{ text: "Amount", value: "amount" },
+			{ text: "Date", value: "createdAt" },
+			{ text: "Type", value: "type" },
+			{ text: "Category", value: "category" },
+			{ text: "Actions", value: "actions", sortable: false },
+		],
+		expenses: [],
+		search: "",
+		editedIndex: -1,
+		editedItem: {
+			index: "",
+		},
 
-    menu1: false,
-    menu2: false
-  }),
-  mounted() {
-    this.getAllExpenses();
-  },
+		startDate: null,
+		endDate: null,
 
-  methods: {
-    getAllExpenses() {
-      axios.get("https://peebu-2020.firebaseio.com/.json").then(
-        response =>
-          (this.expenses = response.data.filter(({ createdAt }) => {
-            if (this.startDate && this.endDate) {
-              const fromDate = new Date(this.startDate).getTime();
-              const toDate = new Date(this.endDate).getTime();
-              if (fromDate && toDate && fromDate <= toDate) {
-                const nextDate = new Date(createdAt).getTime();
-                return nextDate >= fromDate && nextDate <= toDate;
-              }
-              return true;
-            }
-            return true;
-          }))
-      );
-    }
-  }
+		menu1: false,
+		menu2: false,
+	}),
+	mounted() {
+		this.getAllExpenses();
+	},
+
+	methods: {
+		getAllExpenses() {
+			axios.get("https://peebu-2020.firebaseio.com/.json").then(
+				(response) =>
+					(this.expenses = response.data.filter(({ createdAt }) => {
+						if (this.startDate && this.endDate) {
+							const fromDate = new Date(this.startDate).getTime();
+							const toDate = new Date(this.endDate).getTime();
+							if (fromDate && toDate && fromDate <= toDate) {
+								const nextDate = new Date(createdAt).getTime();
+								return (
+									nextDate >= fromDate && nextDate <= toDate
+								);
+							}
+							return true;
+						}
+						return true;
+					}))
+			);
+		},
+
+		itemInfo(item) {
+			this.editedIndex = this.expenses.indexOf(item);
+			this.editedItem = Object.assign({}, item);
+			this.editedItem.createdAt = new Date(
+				this.editedItem.createdAt
+			).toLocaleString();
+			this.infoDialog = true;
+		},
+
+		closeInfoDialog(value) {
+			this.infoDialog = value;
+		},
+	},
 };
 </script>
---
+++ src/components/TransactionInfoDialog.vue
@@ -0,0 +1,82 @@
+<template>
+	<v-dialog v-model="infoDialog" persistent max-width="1000">
+		<v-card>
+			<v-card-title>
+				<span class="headline">Transaction Info</span>
+			</v-card-title>
+			<v-card-text>
+				<v-container>
+					<v-row>
+						<v-col cols="12" sm="6" md="4">
+							<v-text-field
+								v-model="editedItem.entity"
+								readonly
+								label="Entity"
+							></v-text-field>
+						</v-col>
+						<v-col cols="12" sm="6" md="4">
+							<v-text-field
+								v-model="editedItem.amount"
+								readonly
+								label="Amount"
+							></v-text-field>
+						</v-col>
+						<v-col cols="12" sm="6" md="4">
+							<v-text-field
+								v-model="editedItem.createdAt"
+								readonly
+								label="Date"
+							></v-text-field>
+						</v-col>
+						<v-col cols="12" sm="6" md="4">
+							<v-text-field
+								v-model="editedItem.source"
+								readonly
+								label="Source"
+							></v-text-field>
+						</v-col>
+						<v-col cols="12" sm="6" md="4">
+							<v-text-field
+								v-model="editedItem.type"
+								readonly
+								label="Type"
+							></v-text-field>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-card-text>
+			<v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn color="blue darken-1" text @click="close()">Close</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+export default {
+	props: {
+		infoDialog: {
+			type: Boolean,
+		},
+		editedItem: {
+			type: Object,
+		},
+	},
+
+	watch: {
+		infoDialog(val) {
+			val || this.close();
+		},
+	},
+
+	data: () => ({}),
+
+	methods: {
+		close() {
+			this.infoDialog = false;
+			this.$emit("infoDialogClosed", this.infoDialog);
+		},
+	},
+};
+</script>
