ebe8d4b0b85f1bc19d512100dde843b5054dff85
luis ferreira <luispaulo_26@hotmail.com>
2020-06-12 15:35:10 +0100

    Added Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <template>
-  <v-app>
-    <AppBar />
-    <br />
-    <v-content>
-      <v-container>
-        <h1>Welcome!</h1>
-        <h2>Your current balance is: 12345 €</h2>
-        <br />
-        <TransactionDataTable />
-        <br />
-        <h1>Statistics</h1>
-      </v-container>
-    </v-content>
-  </v-app>
+	<v-app>
+		<AppBar />
+		<br />
+		<v-content>
+			<v-container>
+				<h1>Welcome!</h1>
+				<h2>Your current balance is: 12345 €</h2>
+				<br />
+				<TransactionDataTable />
+				<br />
+				<h1>Statistics</h1>
+			</v-container>
+		</v-content>
+		<Footer />
+	</v-app>
 </template>
 
 <script>
 import AppBar from "@/components/AppBar.vue";
 import TransactionDataTable from "@/components/TransactionDataTable.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: "App",
+	name: "App",
 
-  components: {
-    AppBar,
-    TransactionDataTable
-  },
+	components: {
+		AppBar,
+		TransactionDataTable,
+		Footer,
+	},
 
-  data: () => ({
-    //
-  })
+	data: () => ({
+		//
+	}),
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer app color="#58a7c5">
+    <v-row>
+      <div
+        class="white--text"
+      >&copy; {{ new Date().getFullYear() }} Msc Computer Engineering - Software Quality Course</div>
+      <v-spacer></v-spacer>
+      <div class="white--text">Alexandre Silva & Luís Ferreira</div>
+    </v-row>
+  </v-footer>
+</template>
+
+<script></script>
