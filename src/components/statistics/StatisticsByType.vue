74deef1faff94a2b5ce85abcc3c59cd26a90c26c
luis ferreira <luispaulo_26@hotmail.com>
2020-06-14 19:17:57 +0100

    Added data to Transactions by Type Pie  Chart

--- src/components/statistics/StatisticsByType.vue
+++ src/components/statistics/StatisticsByType.vue
@@ -9,7 +9,10 @@
 		<v-row no-gutters>
 			<v-col>
 				<h4>Number of transactions by type</h4>
-				<TransactionsByType />
+				<TransactionsByType
+					v-if="loadedTransactions"
+					:chartdata="chartDataNumber"
+				/>
 			</v-col>
 		</v-row>
 	</div>
@@ -17,14 +20,113 @@
 
 <script>
 import TransactionsByType from "@/components/statistics/TransactionsByTypeChart.vue";
+import axios from "axios";
 
 export default {
 	components: {
 		TransactionsByType,
 	},
 
-	data: () => ({}),
+	data: () => ({
+		loadedTransactions: false,
+		chartDataNumber: null,
 
-	methods: {},
+		expenses: [],
+		number: [],
+
+		numberOfDeposit: 0,
+		numberOfInvoice: 0,
+		numberOfPayment: 0,
+		numberOfWithdrawal: 0,
+	}),
+
+	methods: {
+		async getAllExpenses() {
+			await axios.get("https://peebu-2020.firebaseio.com/.json").then(
+				(response) =>
+					(this.expenses = response.data.filter(({ createdAt }) => {
+						if (this.startDate && this.endDate) {
+							const fromDate = new Date(this.startDate).getTime();
+							const toDate = new Date(this.endDate).getTime();
+							if (fromDate && toDate && fromDate <= toDate) {
+								const nextDate = new Date(createdAt).getTime();
+								return (
+									nextDate >= fromDate && nextDate <= toDate
+								);
+							}
+
+							return true;
+						}
+
+						return true;
+					}))
+			);
+		},
+
+		getTransactionsChart() {
+			this.loadedTransactions = false;
+			this.number = [];
+			this.numberOfDeposit = 0;
+			this.numberOfInvoice = 0;
+			this.numberOfPayment = 0;
+			this.numberOfWithdrawal = 0;
+
+			this.expenses.forEach((expense) => {
+				switch (expense.type) {
+					case "deposit":
+						this.numberOfDeposit += 1;
+						break;
+					case "invoice":
+						this.numberOfInvoice += 1;
+						break;
+					case "payment":
+						this.numberOfPayment += 1;
+						break;
+					case "withdrawal":
+						this.numberOfWithdrawal += 1;
+						break;
+
+					default:
+						break;
+				}
+			});
+			this.number.push(this.numberOfDeposit);
+			this.number.push(this.numberOfInvoice);
+			this.number.push(this.numberOfPayment);
+			this.number.push(this.numberOfWithdrawal);
+
+			this.chartDataNumber = {
+				labels: ["Deposit", "Invoice", "Payment", "Withdrawal"],
+				datasets: [
+					{
+						label: "Number of transactions by type",
+						backgroundColor: [
+							"#0D47A1",
+							"#01579B",
+							"#1565C0",
+							"#0277BD",
+						],
+						data: this.number,
+					},
+				],
+			};
+
+			this.loadedTransactions = true;
+		},
+	},
+
+	async mounted() {
+		await this.getAllExpenses();
+		await this.getTransactionsChart();
+	},
+
+	watch: {
+		expenses: function() {
+			// when the hash prop changes, this function will be fired.
+			console.log(this.expenses.length);
+
+			this.getTransactionsChart();
+		},
+	},
 };
 </script>
