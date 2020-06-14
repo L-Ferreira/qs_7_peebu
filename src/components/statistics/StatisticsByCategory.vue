0e19f672b541f35a57b9ed792ac32c748b188459
luis ferreira <luispaulo_26@hotmail.com>
2020-06-14 17:42:54 +0100

    Added empty Pie Chart (TransactionsByCategoryChart) Component

--- src/components/statistics/StatisticsByCategory.vue
+++ src/components/statistics/StatisticsByCategory.vue
@@ -6,9 +6,21 @@
       </v-col>
     </v-row>
     <br />
+    <v-row no-gutters>
+      <v-col>
+        <h4>Number of transactions by category</h4>
+        <TransactionsByCategory />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-export default {};
+import TransactionsByCategory from "@/components/statistics/TransactionsByCategoryChart.vue";
+
+export default {
+  components: {
+    TransactionsByCategory
+  }
+};
 </script>
