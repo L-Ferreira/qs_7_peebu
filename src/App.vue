1eb00d560e78fd49af0baafa1941a0bf901d8381
Alexandre Silva <2180300@my.ipleiria.pt>
2020-06-14 16:16:56 +0100

    CategoryAlert appears when missing categories

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 			<v-container>
 				<h1>Welcome!</h1>
 				<h2>Your current balance is: 12345 €</h2>
+				<CategoryAlert
+  					v-if="expensesCategorized"
+				/>
 				<br />
 				<TransactionDataTable />
 				<br />
@@ -17,6 +20,8 @@
 </template>
 
 <script>
+import axios from "axios";
+import CategoryAlert from "@/components/CategoryAlert.vue";
 import AppBar from "@/components/AppBar.vue";
 import TransactionDataTable from "@/components/TransactionDataTable.vue";
 import Footer from "@/components/Footer.vue";
@@ -25,13 +30,36 @@ export default {
 	name: "App",
 
 	components: {
+		CategoryAlert,
 		AppBar,
 		TransactionDataTable,
 		Footer,
 	},
 
 	data: () => ({
-		//
+		expensesCategorized: false,
+        expenses: [],
 	}),
+
+	async created() {
+        await this.getAllExpenses();
+    },
+
+	methods: {
+		async getAllExpenses() {
+            await axios.get("https://peebu-2020.firebaseio.com/.json").then(
+                (response) => (
+                    (this.expenses = response.data),
+                    this.expenses.some((expense) => {
+                        if (expense.category == "none") {
+                            return (this.expensesCategorized = true);
+                        } else {
+                            this.expensesCategorized = false;
+                        }
+                    })
+                )
+            );
+        },
+	}
 };
 </script>
